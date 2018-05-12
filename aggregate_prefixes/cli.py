6301624c7899830465aa78e9ba11259a14f5f91c
Marco Marzetti <marco@lamehost.it>
2018-05-13 00:22:14 +0200

    added CLI arguments

--- aggregate_prefixes/cli.py
+++ aggregate_prefixes/cli.py
@@ -6,17 +6,11 @@ Provides CLI interface for package aggregate-prefixes
 
 from __future__ import absolute_import
 from __future__ import print_function
-import fileinput
+import argparse
 import sys
-try:
-    # Python 2
-    from future_builtins import filter
-except ImportError:
-    # Python 3
-    pass
 
 from aggregate_prefixes.aggregate_prefixes import aggregate_prefixes
-
+from aggregate_prefixes import __version__ as VERSION
 
 def main():
     """
@@ -26,9 +20,38 @@ def main():
     Returns a sorted list of aggregates to STDOUT.
     """
 
-    prefixes = filter(None, set([_.strip() for _ in fileinput.input()]))
+    parser = argparse.ArgumentParser(
+        prog='aggregate-prefixes',
+        description='Aggregates IPv4 or IPv6 prefixes from file or STDIN'
+    )
+    parser.add_argument(
+        'prefixes',
+        type=argparse.FileType('r'),
+        default=sys.stdin
+    )
+    parser.add_argument(
+        '--max-length', '-m',
+        metavar='LENGTH',
+        type=int,
+        help='Discard longer prefixes prior to processing',
+        default=128
+    )
+    parser.add_argument(
+        '--verbose', '-v',
+        help='Display verbose information about the optimisations',
+        action='store_true'
+    )
+    parser.add_argument(
+        '--version', '-V',
+        action='version',
+        version='%(prog)s ' + VERSION
+    )
+    args = parser.parse_args()
+
+    # Read and cleanup prefixes
+    prefixes = [_ for _ in set([_.strip() for _ in args.prefixes]) if _]
     try:
-        aggregates = aggregate_prefixes(prefixes)
+        aggregates = aggregate_prefixes(prefixes, args.max_length, args.verbose)
     except (ValueError, TypeError) as error:
         sys.exit(error)
     print('\n'.join(aggregates))
