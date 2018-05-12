0cfe5eba42e0db62dca757acd27731cfe984add5
Marco Marzetti <marco@lamehost.it>
2018-05-12 17:18:24 +0200

    now runs on python 3.6

--- aggregate_prefixes/cli.py
+++ aggregate_prefixes/cli.py
@@ -5,9 +5,15 @@ Provides CLI interface for package aggregate-prefixes
 """
 
 from __future__ import absolute_import
+from __future__ import print_function
 import fileinput
 import sys
-from itertools import ifilter
+try:
+    # Python 2
+    from future_builtins import filter
+except ImportError:
+    # Python 3
+    pass
 
 from aggregate_prefixes.aggregate_prefixes import aggregate_prefixes
 
@@ -20,12 +26,12 @@ def main():
     Returns a sorted list of aggregates to STDOUT.
     """
 
-    prefixes = ifilter(None, set([_.strip() for _ in fileinput.input()]))
+    prefixes = filter(None, set([_.strip() for _ in fileinput.input()]))
     try:
         aggregates = aggregate_prefixes(prefixes)
-    except (ValueError, TypeError), error:
+    except (ValueError, TypeError) as error:
         sys.exit(error)
-    print '\n'.join(aggregates)
+    print('\n'.join(aggregates))
 
 
 if __name__ == '__main__':
