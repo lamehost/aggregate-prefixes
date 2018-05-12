127b6360f5da53e2d341bebbf599e21a748e3414
Marco Marzetti <marco@lamehost.it>
2018-05-12 13:51:41 +0200

    docstring added

--- aggregate_prefixes/cli.py
+++ aggregate_prefixes/cli.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Provides CLI interface for package aggregate-prefixes
+"""
 
 from __future__ import absolute_import
 import fileinput
@@ -8,8 +12,14 @@ from itertools import ifilter
 from aggregate_prefixes.aggregate_prefixes import aggregate_prefixes
 
 
-
 def main():
+    """
+    Aggregates IPv4 or IPv6 prefixes from file or STDIN.
+
+    Reads a list of unsorted IPv4 or IPv6 prefixes from a file or STDIN.
+    Returns a sorted list of aggregates to STDOUT.
+    """
+
     prefixes = ifilter(None, set([_.strip() for _ in fileinput.input()]))
     try:
         aggregates = aggregate_prefixes(prefixes)
