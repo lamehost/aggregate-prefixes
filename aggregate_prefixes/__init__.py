868542fce55ac67336a852acd47b731f66b020b3
Marco Marzetti <marco@lamehost.it>
2018-05-31 20:55:02 +0200

    Added Classifiers

--- aggregate_prefixes/__init__.py
+++ aggregate_prefixes/__init__.py
@@ -19,3 +19,13 @@ __version__ = "0.3"
 __author__ = "Marco Marzetti"
 __author_email__ = "marco@lamehost.it"
 __url__ = "https://github.com/lamehost/aggregate-prefixes/"
+__description__ = "Fast IPv4 and IPv6 prefix aggregator written in Python."
+__license__ = "MIT"
+__classifiers__ = [
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: System :: Networking',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.6'
+],
