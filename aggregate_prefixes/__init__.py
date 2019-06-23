ae641593506bcd5e375962d803755fdca9f79d44
Marco Marzetti <marco@lamehost.it>
2019-06-23 08:46:04 +0800

    Added licensing information

--- aggregate_prefixes/__init__.py
+++ aggregate_prefixes/__init__.py
@@ -1,5 +1,28 @@
 # -*- coding: utf-8 -*-
 
+# MIT License
+
+# Copyright (c) 2018, Marco Marzetti <marco@lamehost.it>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 """
 Aggregates IPv4 or IPv6 prefixes.
 
@@ -20,7 +43,7 @@ from __future__ import absolute_import
 from aggregate_prefixes.aggregate_prefixes import aggregate_prefixes
 
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 __author__ = "Marco Marzetti"
 __author_email__ = "marco@lamehost.it"
 __url__ = "https://github.com/lamehost/aggregate-prefixes/"
