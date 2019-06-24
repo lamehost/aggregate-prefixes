d5bb18d39a7c2212639f9ab198997c2fd8568bdc
Marco Marzetti <marco@lamehost.it>
2019-06-24 14:38:31 +0200

    cli.py renamed to __main__.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'aggregate-prefixes = aggregate_prefixes.cli:main',
+            'aggregate-prefixes = aggregate_prefixes.__main__:main',
         ],
     },
     long_description=README,
