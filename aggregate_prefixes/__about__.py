85d24c44c6df10ac76f27f742627cf5f51545e05
Marco Marzetti <marco@lamehost.it>
2019-06-24 13:51:24 +0200

    first commit

---
+++ aggregate_prefixes/__about__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# MIT License
+
+# Copyright (c) 2019, Marco Marzetti <marco@lamehost.it>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+__version__ = "0.6.1"
+__author__ = "Marco Marzetti"
+__author_email__ = "marco@lamehost.it"
+__url__ = "https://github.com/lamehost/aggregate-prefixes/"
+__description__ = "Fast IPv4 and IPv6 prefix aggregator written in Python."
+__license__ = "MIT"
+__classifiers__ = [
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: System :: Networking',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.6'
+]
