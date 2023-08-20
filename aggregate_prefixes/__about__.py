953949c90a2e1e9d3a012580e1bcbe330343de32
Marco Marzetti <marco@lamehost.it>
2023-08-20 10:25:40 +0200

    Fixed typo

--- aggregate_prefixes/__about__.py
+++ aggregate_prefixes/__about__.py
@@ -26,7 +26,7 @@
 """Provide version info to the package"""
 
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 __author__ = "Marco Marzetti"
 __author_email__ = "marco@lamehost.it"
 __url__ = "https://github.com/lamehost/aggregate-prefixes/"
